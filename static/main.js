b277dca8d18d36217355e693f613aa9ef4d59d95
s-mahali <mahalisoumen839@gmail.com>
2025-08-05 12:58:09 +0530

    Day 4 Created a Echo bot which record user audio and play it back

--- static/main.js
+++ static/main.js
@@ -1,6 +1,11 @@
 const inputField = document.getElementById("inputField");
 const generateButton = document.getElementById("generateButton");
 const audioPlayer = document.getElementById("audioPlayer");
+const recordButton = document.getElementById("recordButton");
+const stopRecordButton = document.getElementById("stopButton");
+const recordingPlayer = document.getElementById("recordingPlayer");
+const recorderContainer = document.getElementById("recorderContainer");
+console.log("recordButton", recordButton);
 
 let content = "";
 
@@ -15,19 +20,72 @@ async function fetchAudioContent(e) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ text: content }),
-    })
-    if(response) {
+    });
+    if (response) {
       const audioResponse = await response.json();
       audioPlayer.src = audioResponse;
       audioPlayer.play();
       inputField.value = "";
-      
-      
     }
-   
   } catch (error) {
     console.error("error fetching audio content:", error?.message);
   }
 }
 
 generateButton.addEventListener("click", fetchAudioContent);
+
+//Day-4
+if (navigator.mediaDevices) {
+  console.log("mediaDevices", navigator.mediaDevices);
+}
+
+const constraints = { audio: true };
+let chunks = [];
+
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then((stream) => {
+    const mediaRecorder = new MediaRecorder(stream);
+
+    recordButton.onclick = () => {
+      console.log("clicked");
+      mediaRecorder.start();
+      console.log("started recording", mediaRecorder.state);
+      recordButton.style.background = "red";
+      recordButton.innerText = "Recording in Progress";
+      recordButton.disabled = true;
+      recordButton.style.cursor = "not-allowed";
+      recordButton.style.color = "black";
+    };
+
+    stopRecordButton.onclick = () => {
+      mediaRecorder.stop();
+      console.log("stopped recording", mediaRecorder.state);
+      recordButton.style.background = "green";
+      alert("record stopped!");
+      recordButton.disabled = false;
+      recordButton.style.cursor = "pointer";
+      recordButton.style.color = "";
+      recordButton.style.background = "";
+      stopRecordButton.style.color = "black";
+      recordButton.textContent = "Record Audio";
+    };
+
+    mediaRecorder.onstop = (e) => {
+      console.log("data available after MediaRecorder.stop() called.");
+
+      const blob = new Blob(chunks, { type: "audio/ogg; codecs = opus" });
+      chunks = [];
+      const audioURL = URL.createObjectURL(blob);
+      recordingPlayer.src = audioURL;
+      recordingPlayer.play();
+      console.log("recorder stopped");
+    };
+
+    mediaRecorder.ondataavailable = (e) => {
+      chunks.push(e.data);
+    };
+  })
+  .catch((err) => {
+    console.error(`The following error occurred: ${err}`);
+  });
